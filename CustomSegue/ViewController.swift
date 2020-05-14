f027a8a913c746dd5904ea0b17cef7475315ba32
gonnbe1106 <gonnbe1106@yahoo.co.jp>
2020-05-14 21:42:19 +0900

    Initial Commit

---
+++ CustomSegue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSegue
+//
+//  Created by 五十嵐伸雄 on 2020/05/14.
+//  Copyright © 2020 五十嵐伸雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
